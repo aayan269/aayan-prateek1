ff3ef0e63485ea9acec6cb19fe10841b1007b990
aayan269 <ahmadaayan269@gmail.com>
2022-10-16 22:09:09 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <main className='main'>
+    <About />
+    </main>
+    
+    </>
+      
+      
+  
+  );
+}
+
+export default App;
